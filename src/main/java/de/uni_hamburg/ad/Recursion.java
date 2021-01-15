fe6c979149ab4c826a439e6a05ef39418a6758a9
Valerij Dobler <vd@wps.de>
2021-01-16 00:46:18 +0100

    Recursive fibonacci, recursive fibonacci with memoization and iterative fibonacci with dyn

--- src/main/java/de/uni_hamburg/ad/Recursion.java
+++ src/main/java/de/uni_hamburg/ad/Recursion.java
@@ -1,5 +1,8 @@
 package de.uni_hamburg.ad;
 
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Recursion {
@@ -11,6 +14,28 @@ public class Recursion {
         }
         accumulator[n-1] = rnd.nextBoolean() ? "red" : "blue";
         return tailRecursion(n-1, accumulator, rnd);
+    }
 
+    long fib(int n) {
+        if (n == 0 || n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    BigInteger fibFast(int n, Map<Integer, BigInteger> dyn) {
+        if (n == 0 || n == 1) return BigInteger.ONE;
+        if (!dyn.containsKey(n))
+            dyn.put(n, fibFast(n - 1, dyn).add(fibFast(n - 2, dyn)));
+        return dyn.get(n);
+    }
+
+    BigInteger fibFastIter(int n) {
+        Map<Integer, BigInteger> dyn = new HashMap<>();
+        dyn.put(0, BigInteger.ONE);
+        dyn.put(1, BigInteger.ONE);
+        int i = 1;
+        while(n > i) {
+            dyn.put(++i, dyn.get(i - 1).add(dyn.get(i - 2)));
+        }
+        return dyn.get(n);
     }
 }
