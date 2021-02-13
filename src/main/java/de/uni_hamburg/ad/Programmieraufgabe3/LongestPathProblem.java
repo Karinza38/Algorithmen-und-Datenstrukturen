4d2a10b91c49ed845f2e3d8d7bf9dd571b70c8cf
Valerij Dobler <vd@wps.de>
2021-02-13 19:16:49 +0100

    Refactors LongestPathProblem

--- src/main/java/de/uni_hamburg/ad/Programmieraufgabe3/LongestPathProblem.java
+++ src/main/java/de/uni_hamburg/ad/Programmieraufgabe3/LongestPathProblem.java
@@ -5,57 +5,49 @@ import java.util.ArrayList;
 public class LongestPathProblem {
 
   // Function that returns the longest path
-  
+
   public int calc(int N, int[][] A) {
     //initialize
-    ArrayList<ArrayList<Integer>> edges = new ArrayList<>();
-    for (int i = 0; i <= N; i++)
-    {
-      edges.add(new ArrayList<>());
-    }
-    for(int[] singleEdge : A){
-      int startKnoten = singleEdge[0];
-      int endKnoten = singleEdge[1];
-      edges.get(startKnoten).add(endKnoten);
-    }
-    
-    ArrayList<ArrayList<Integer>> adj = edges;
+    ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+    for (int i = 0; i <= N; i++) adj.add(new ArrayList<>());
+
+    for(int[] edge : A) adj.get(edge[0]).add(edge[1]);
+
     // Dp array
     int[] dp = new int[N + 1];
-    
+
     // Visited array to know if the node
     // has been visited previously or not
     boolean[] visited = new boolean[N + 1];
-    
+
     // Call DFS for every unvisited vertex
     for (int i = 1; i <= N; i++) {
       if (!visited[i])
         dfs(i, adj, dp, visited);
     }
-    
+
     int ans = 0;
-    
+
     // Traverse and find the maximum of all dp[i]
     for (int i = 1; i <= N; i++) {
       ans = Math.max(ans, dp[i]);
     }
     return ans;
   }
-  
-  void dfs(int node, ArrayList<ArrayList<Integer>> adj, int dp[],
-      boolean visited[]) {
+
+  void dfs(int root, ArrayList<ArrayList<Integer>> adj, int[] dp,  boolean[] visited) {
     // Mark as visited
-    visited[node] = true;
+    visited[root] = true;
 
     // Traverse for all its children
-    for (int i = 0; i < adj.get(node).size(); i++) {
-
+    for (Integer neighbour : adj.get(root))
+    {
       // If not visited
-      if (!visited[adj.get(node).get(i)])
-        dfs(adj.get(node).get(i), adj, dp, visited);
+      if (!visited[neighbour])
+        dfs(neighbour, adj, dp, visited);
 
       // Store the max of the paths
-      dp[node] = Math.max(dp[node], 1 + dp[adj.get(node).get(i)]);
+      dp[root] = Math.max(dp[root], 1 + dp[neighbour]);
     }
   }
 
