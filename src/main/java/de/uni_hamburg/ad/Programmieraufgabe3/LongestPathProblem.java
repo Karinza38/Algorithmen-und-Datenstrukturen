bf2c9572435a9d808a1a6cb85cc7419d2113017b
Valerij Dobler <vd@wps.de>
2021-02-13 14:54:29 +0100

    changed to java8

--- src/main/java/de/uni_hamburg/ad/Programmieraufgabe3/LongestPathProblem.java
+++ src/main/java/de/uni_hamburg/ad/Programmieraufgabe3/LongestPathProblem.java
@@ -5,21 +5,21 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class LongestPathProblem {
-  
+
   int calc(int N, int[][] A) {
-    var adj = parseAdj(A);
+    ArrayList<ArrayList<Integer>> adj = parseAdj(A);
     int[] dp = new int[N + 1];
-    
+
     boolean[] visited = new boolean[N + 1];
-    
+
     // Call DFS for every unvisited vertex
     for (int i = 1; i <= N; i++) {
       if (!visited[i])
         dfs(i, adj, dp, visited);
     }
-    
+
     int ans = 0;
-    
+
     // Traverse and find the maximum of all dp[i]
     for (int i = 1; i <= N; i++) {
       ans = Math.max(ans, dp[i]);
@@ -27,28 +27,28 @@ public class LongestPathProblem {
     return ans;
   }
 
-  private List<List<Integer>> parseAdj(int[][] a)
+  private ArrayList<ArrayList<Integer>> parseAdj(int[][] a)
   {
-    var adj = new ArrayList<List<Integer>>(9001);
-    for (var edges : a) {
+    ArrayList<ArrayList<Integer>> adj = new ArrayList<>(9001);
+    for (int[] edges : a) {
       final int startNode = edges[0];
       final int endNode = edges[1];
-      while(adj.size() <= startNode) adj.add(new LinkedList<>());
+      while(adj.size() <= startNode) adj.add(new ArrayList<>());
       adj.get(startNode).add(endNode);
     }
     return adj;
   }
 
-  void dfs(int node, List<List<Integer>> adj, int[] dp, boolean[] visited) {
+  void dfs(int node, ArrayList<ArrayList<Integer>> adj, int[] dp, boolean[] visited) {
     visited[node] = true;
     // Traverse for all its children
     if (adj.size() <= node) return;
-    for (var neighbour : adj.get(node)) {
-      
+    for (Integer neighbour : adj.get(node)) {
+
       // If not visited
       if (!visited[neighbour])
         dfs(neighbour, adj, dp, visited);
-      
+
       // Store the max of the paths
       dp[node] = Math.max(dp[node], 1 + neighbour);
     }
