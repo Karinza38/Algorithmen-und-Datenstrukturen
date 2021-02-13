188dd9eba4b44b6b83c8525e412d0793bc4e72db
Robin <robinwannags@gmx.de>
2021-02-13 16:20:51 +0100

    Programmieraufgabe 3 Lösung für LongestPathProblem

--- src/main/java/de/uni_hamburg/ad/Programmieraufgabe3/LongestPathProblem.java
+++ src/main/java/de/uni_hamburg/ad/Programmieraufgabe3/LongestPathProblem.java
@@ -1,57 +1,62 @@
 package de.uni_hamburg.ad.Programmieraufgabe3;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 public class LongestPathProblem {
 
-  int calc(int N, int[][] A) {
-    ArrayList<ArrayList<Integer>> adj = parseAdj(A);
+  // Function that returns the longest path
+  
+  public int calc(int N, int[][] A) {
+    //initialize
+    ArrayList<ArrayList<Integer>> edges = new ArrayList<>();
+    for (int i = 0; i <= N; i++)
+    {
+      edges.add(new ArrayList<>());
+    }
+    for(int[] singleEdge : A){
+      int startKnoten = singleEdge[0];
+      int endKnoten = singleEdge[1];
+      edges.get(startKnoten).add(endKnoten);
+    }
+    
+    ArrayList<ArrayList<Integer>> adj = edges;
+    // Dp array
     int[] dp = new int[N + 1];
-
+    
+    // Visited array to know if the node
+    // has been visited previously or not
     boolean[] visited = new boolean[N + 1];
-
+    
     // Call DFS for every unvisited vertex
     for (int i = 1; i <= N; i++) {
       if (!visited[i])
         dfs(i, adj, dp, visited);
     }
-
+    
     int ans = 0;
-
+    
     // Traverse and find the maximum of all dp[i]
     for (int i = 1; i <= N; i++) {
       ans = Math.max(ans, dp[i]);
     }
     return ans;
   }
-
-  private ArrayList<ArrayList<Integer>> parseAdj(int[][] a)
-  {
-    ArrayList<ArrayList<Integer>> adj = new ArrayList<>(9001);
-    for (int[] edges : a) {
-      final int startNode = edges[0];
-      final int endNode = edges[1];
-      while(adj.size() <= startNode) adj.add(new ArrayList<>());
-      adj.get(startNode).add(endNode);
-    }
-    return adj;
-  }
-
-  void dfs(int node, ArrayList<ArrayList<Integer>> adj, int[] dp, boolean[] visited) {
+  
+  void dfs(int node, ArrayList<ArrayList<Integer>> adj, int dp[],
+      boolean visited[]) {
+    // Mark as visited
     visited[node] = true;
+
     // Traverse for all its children
-    if (adj.size() <= node) return;
-    for (Integer neighbour : adj.get(node)) {
+    for (int i = 0; i < adj.get(node).size(); i++) {
 
       // If not visited
-      if (!visited[neighbour])
-        dfs(neighbour, adj, dp, visited);
+      if (!visited[adj.get(node).get(i)])
+        dfs(adj.get(node).get(i), adj, dp, visited);
 
       // Store the max of the paths
-      dp[node] = Math.max(dp[node], 1 + neighbour);
+      dp[node] = Math.max(dp[node], 1 + dp[adj.get(node).get(i)]);
     }
   }
-  
+
 }
