a5895a911c45fd346fa0f2aec65d5f7453efefe2
Valerij Dobler <vd@wps.de>
2021-02-13 14:57:21 +0100

    Adds acceptance tests

--- src/test/java/de/uni_hamburg/ad/Programmieraufgabe3/LongestPathProblemTest.java
+++ src/test/java/de/uni_hamburg/ad/Programmieraufgabe3/LongestPathProblemTest.java
@@ -23,4 +23,43 @@ class LongestPathProblemTest {
     // Arrange
     assertThat(result, is(2));
   }
+
+  @Test
+  void calc2() {
+    // Arrange
+    int[][] arr = new int[][]{{3, 4}, {1, 5}, {1, 4}, {4, 5}, {2, 5}};
+    int N = 5;
+
+    // Act
+    final int result = lpp.calc(N, arr);
+
+    // Arrange
+    assertThat(result, is(2));
+  }
+
+  @Test
+  void calc3() {
+    // Arrange
+    int[][] arr = new int[][]{{1, 2}, {3, 4}, {1, 5}, {3, 7}, {4, 6}, {5, 7}, {2, 3}, {6, 7}, {2, 6}, {5, 6}, {1, 6}, {2, 5}, {4, 7}};
+    int N = 7;
+
+    // Act
+    final int result = lpp.calc(N, arr);
+
+    // Arrange
+    assertThat(result, is(5));
+  }
+
+  @Test
+  void calc4() {
+    // Arrange
+    int[][] arr = new int[][]{{2, 4}, {4, 6}, {2, 3}, {5, 6}, {3, 6}, {1, 6}, {1, 3}, {3, 5}};
+    int N = 6;
+
+    // Act
+    final int result = lpp.calc(N, arr);
+
+    // Arrange
+    assertThat(result, is(3));
+  }
 }
