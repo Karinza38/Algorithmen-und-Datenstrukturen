b2a695214d506b214bb2127437e10702dbbec6b4
Valerij Dobler <vd@wps.de>
2021-02-14 01:34:29 +0100

    Adds quiet useless dp solution for LCS

--- src/main/java/de/uni_hamburg/ad/dp/LongestCommonSubsequence.java
+++ src/main/java/de/uni_hamburg/ad/dp/LongestCommonSubsequence.java
@@ -1,6 +1,7 @@
 package de.uni_hamburg.ad.dp;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class LongestCommonSubsequence
@@ -29,5 +30,38 @@ public class LongestCommonSubsequence
 		List<Character> rightResult = solveRec(source, target.substring(0, endTargetIndex));
 
 		return leftResult.size() > rightResult.size() ? leftResult : rightResult;
+	}
+
+
+	public String solveDP(String source, String target) {
+		StringBuilder result = new StringBuilder();
+		solveRecDP(source, target, new HashMap<>()).forEach(result::append);
+		return result.toString();
+	}
+
+	private List<Character> solveRecDP(String source, String target, HashMap<String, ArrayList<Character>> dp) {
+		// If one of the parts is empty, they can't have anything common left to compare
+		if (source.length() * target.length() == 0) {
+			return new ArrayList<>();
+		}
+		if (dp.containsKey(source+","+target))
+			return dp.get(source+","+target);
+
+		final int endSourceIndex = source.length() - 1;
+		final int endTargetIndex = target.length() - 1;
+		if (source.charAt(endSourceIndex) == target.charAt(endTargetIndex)) {
+			ArrayList<Character> result = new ArrayList<>();
+			result.add(source.charAt(endSourceIndex));
+			List<Character> middle = solveRec(source.substring(0, endSourceIndex), target.substring(0, endTargetIndex));
+			result.addAll(0, middle);
+			dp.put(source+","+target, result);
+			return result;
+		}
+		List<Character> leftResult = solveRec(source.substring(0, endSourceIndex), target);
+		List<Character> rightResult = solveRec(source, target.substring(0, endTargetIndex));
+
+		final ArrayList<Character> result = (ArrayList<Character>)(leftResult.size() > rightResult.size() ? leftResult : rightResult);
+		dp.put(source+","+target, result);
+		return result;
 	}
 }
--- src/test/java/de/uni_hamburg/ad/dp/LongestCommonSubsequenceTest.java
+++ src/test/java/de/uni_hamburg/ad/dp/LongestCommonSubsequenceTest.java
@@ -1,10 +1,10 @@
 package de.uni_hamburg.ad.dp;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class LongestCommonSubsequenceTest
 {
@@ -74,5 +74,101 @@ class LongestCommonSubsequenceTest
 
 		// Assert
 		assertThat(solution, is("12356"));
+	}
+
+	@Test
+	@Disabled // 5m 23s 813ms
+	void solve_givenBigTestCase_thatResultIsCorrect() {
+		// Arrange
+		var source = "lRk3W1I9WxwL8u3l2GA4";
+		var target = "3In9qxKcLuN963Yxlvq4";
+
+		// Act
+		final String solution = lcs.solve(source, target);
+
+		// Assert
+		assertThat(solution, is("3I9xLu3l4"));
+	}
+
+	/**
+	 * DP
+	 */
+	@Test
+	void solveDP_givenEmptySource_thatResultIsAlsoEmpty() {
+		// Arrange
+		var source = "";
+		var target = "abc";
+
+		// Act
+		final String solution = lcs.solveDP(source, target);
+
+		// Assert
+		assertThat(solution, is(""));
+	}
+
+	@Test
+	void solveDP_givenEmptyTarget_thatResultIsAlsoEmpty() {
+		// Arrange
+		var source = "abc";
+		var target = "";
+
+		// Act
+		final String solution = lcs.solveDP(source, target);
+
+		// Assert
+		assertThat(solution, is(""));
+	}
+
+	@Test
+	void solveDP_givenConsecutiveMatchingString_thatResultIsAbc() {
+		// Arrange
+		var source = "abcdef";
+		var target = "abc";
+
+		// Act
+		final String solution = lcs.solveDP(source, target);
+
+		// Assert
+		assertThat(solution, is("abc"));
+	}
+
+	@Test
+	void solveDP_givenOnlyPatchedMatching_thatResultIsAcf() {
+		// Arrange
+		var source = "abc";
+		var target = "";
+
+		// Act
+		final String solution = lcs.solveDP(source, target);
+
+		// Assert
+		assertThat(solution, is(""));
+	}
+
+	@Test
+	void solveDP_givenMixedAndSortedNumbers_thatResultIs12356() {
+		// Arrange
+		var source = "132535365";
+		var target = "123456789";
+
+		// Act
+		final String solution = lcs.solveDP(source, target);
+
+		// Assert
+		assertThat(solution, is("12356"));
+	}
+
+	@Test
+	@Disabled // 5m 8s 232ms
+	void solveDP_givenBigTestCase_thatResultIsCorrect() {
+		// Arrange
+		var source = "lRk3W1I9WxwL8u3l2GA4";
+		var target = "3In9qxKcLuN963Yxlvq4";
+
+		// Act
+		final String solution = lcs.solveDP(source, target);
+
+		// Assert
+		assertThat(solution, is("3I9xLu3l4"));
 	}
 }
