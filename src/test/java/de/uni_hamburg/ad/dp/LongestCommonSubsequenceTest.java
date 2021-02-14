ba15f72c2709dfe44fd1c261a43a6d261e7702ed
Valerij Dobler <vd@wps.de>
2021-02-14 22:44:24 +0100

    Refactors tests

--- src/test/java/de/uni_hamburg/ad/dp/LongestCommonSubsequenceTest.java
+++ src/test/java/de/uni_hamburg/ad/dp/LongestCommonSubsequenceTest.java
@@ -2,6 +2,8 @@ package de.uni_hamburg.ad.dp;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,73 +13,29 @@ class LongestCommonSubsequenceTest
 
 	LongestCommonSubsequence lcs = new LongestCommonSubsequence();
 
-	@Test
-	void solve_givenEmptySource_thatResultIsAlsoEmpty() {
+	@ParameterizedTest
+	@CsvSource({",abc,",
+			"abc,,",
+			"abcdef,abc,abc",
+			"abcdef,acf,acf",
+			"132535365,123456789,12356",
+			"abcdefghijklmnopq,abcdefghijklmnopq,abcdefghijklmnopq",
+			"nothardlythefinaltest,zzzfinallyzzz,final"})
+	void solve_givenEmptySource_thatResultIsAlsoEmpty(String source, String target, String expected) {
 		// Arrange
-		var source = "";
-		var target = "abc";
+		source = source == null ? "" : source;
+		target = target == null ? "" : target;
+		expected = expected == null ? "" : expected;
 
 		// Act
 		final String solution = lcs.solve(source, target);
 
 		// Assert
-		assertThat(solution, is(""));
+		assertThat(solution, is(expected));
 	}
 
 	@Test
-	void solve_givenEmptyTarget_thatResultIsAlsoEmpty() {
-		// Arrange
-		var source = "abc";
-		var target = "";
-
-		// Act
-		final String solution = lcs.solve(source, target);
-
-		// Assert
-		assertThat(solution, is(""));
-	}
-
-	@Test
-	void solve_givenConsecutiveMatchingString_thatResultIsAbc() {
-		// Arrange
-		var source = "abcdef";
-		var target = "abc";
-
-		// Act
-		final String solution = lcs.solve(source, target);
-
-		// Assert
-		assertThat(solution, is("abc"));
-	}
-
-	@Test
-	void solve_givenOnlyPatchedMatching_thatResultIsAcf() {
-		// Arrange
-		var source = "abc";
-		var target = "";
-
-		// Act
-		final String solution = lcs.solve(source, target);
-
-		// Assert
-		assertThat(solution, is(""));
-	}
-
-	@Test
-	void solve_givenMixedAndSortedNumbers_thatResultIs12356() {
-		// Arrange
-		var source = "132535365";
-		var target = "123456789";
-
-		// Act
-		final String solution = lcs.solve(source, target);
-
-		// Assert
-		assertThat(solution, is("12356"));
-	}
-
-	@Test
-	@Disabled // 5m 23s 813ms
+	@Disabled("5m 23s 813ms")
 	void solve_givenBigTestCase_thatResultIsCorrect() {
 		// Arrange
 		var source = "lRk3W1I9WxwL8u3l2GA4";
@@ -90,85 +48,4 @@ class LongestCommonSubsequenceTest
 		assertThat(solution, is("3I9xLu3l4"));
 	}
 
-	/**
-	 * DP
-	 */
-	@Test
-	void solveDP_givenEmptySource_thatResultIsAlsoEmpty() {
-		// Arrange
-		var source = "";
-		var target = "abc";
-
-		// Act
-		final String solution = lcs.solveDP(source, target);
-
-		// Assert
-		assertThat(solution, is(""));
-	}
-
-	@Test
-	void solveDP_givenEmptyTarget_thatResultIsAlsoEmpty() {
-		// Arrange
-		var source = "abc";
-		var target = "";
-
-		// Act
-		final String solution = lcs.solveDP(source, target);
-
-		// Assert
-		assertThat(solution, is(""));
-	}
-
-	@Test
-	void solveDP_givenConsecutiveMatchingString_thatResultIsAbc() {
-		// Arrange
-		var source = "abcdef";
-		var target = "abc";
-
-		// Act
-		final String solution = lcs.solveDP(source, target);
-
-		// Assert
-		assertThat(solution, is("abc"));
-	}
-
-	@Test
-	void solveDP_givenOnlyPatchedMatching_thatResultIsAcf() {
-		// Arrange
-		var source = "abc";
-		var target = "";
-
-		// Act
-		final String solution = lcs.solveDP(source, target);
-
-		// Assert
-		assertThat(solution, is(""));
-	}
-
-	@Test
-	void solveDP_givenMixedAndSortedNumbers_thatResultIs12356() {
-		// Arrange
-		var source = "132535365";
-		var target = "123456789";
-
-		// Act
-		final String solution = lcs.solveDP(source, target);
-
-		// Assert
-		assertThat(solution, is("12356"));
-	}
-
-	@Test
-	@Disabled // 5m 8s 232ms
-	void solveDP_givenBigTestCase_thatResultIsCorrect() {
-		// Arrange
-		var source = "lRk3W1I9WxwL8u3l2GA4";
-		var target = "3In9qxKcLuN963Yxlvq4";
-
-		// Act
-		final String solution = lcs.solveDP(source, target);
-
-		// Assert
-		assertThat(solution, is("3I9xLu3l4"));
-	}
 }
